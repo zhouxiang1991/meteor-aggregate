2e5fca5fcca69a6fe8cd8a9545ba47a7a8865117
zhouxiang1991 <zhouxiang1991@caifujilu.com>
2018-01-11 15:13:19 +0800

    init

---
+++ meteor-aggregate-tests.js
@@ -0,0 +1,15 @@
+// Import Tinytest from the tinytest Meteor package.
+// import { Tinytest } from "meteor/tinytest";
+
+// Import and rename a variable exported by meteor-aggregate.js.
+// import Aggregate from "meteor/meteor-aggregate";
+// const Aggregate = require('./meteor-aggregate.js')
+// const aggregate = new Aggregate();
+// aggregate.unwind('aa');
+// console.log(aggregate.pipeline());
+
+// Write your tests here!
+// Here is an example.
+// Tinytest.add('meteor-aggregate - example', function (test) {
+  // test.equal(packageName, "meteor-aggregate");
+// });
